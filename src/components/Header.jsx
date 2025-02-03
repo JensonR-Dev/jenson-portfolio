ee82f64aeac4585b3a6239e3a7a4a1e04bd18c50
JensonR-Dev <r_jenson@outlook.com>
2025-02-03 07:10:16 +0530

    Portfolio start

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+
+export default function Header(){
+    return <header className="portfolio-header">
+        <div className="container">
+            <div className="pf-head-grid">
+                <div className="pf-head-cnt">
+                <h1>I&rsquo;m Jenson R</h1>
+                <p>Creative and detail-oriented Web Designer with 4 Years of experience translating client visions into innovative and visually appealing websites. Proficient in front-end development languages and tools such as HTML, CSS, JavaScript, Wordpress, Bootstrap, and Webflow, with a experience in design softwares like Abobe Photoshop, Illustrator and Figma. Adept at collaborating with cross-functional teams to deliver high-quality digital solutions that meet client objectives. Proven track record of managing multiple projects simultaneously, meeting tight deadlines, and exceeding client expectations. Passionate about staying abreast of industry trends and emerging technologies to consistently deliver cutting-edge designs.</p>
+                </div>
+            </div>
+        </div>
+    </header>
+}
