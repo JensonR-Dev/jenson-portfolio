665419c7db0fe323f2282fb4b9f0f9cbf00dc9d1
JensonR-Dev <r_jenson@outlook.com>
2025-02-04 19:36:28 +0530

    cta

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 import ProfileImage from '../assets/Jenson-Profile.jpg';
-import ResumePDF from '../assets/Jenson-2024-Resume.pdf'
+import ResumePDF from '../assets/Jenson-2024-Resume.pdf';
+import Navbar from './Navbar/Navbar';
 
 export default function Header() {
   return (
     <header className="portfolio-hero">
       <div className="container">
+        <Navbar />
         <div className="pf-head-grid">
           <div className="pf-head-cnt">
             <p className='greeting-msg'>Greetings </p>
@@ -12,12 +14,12 @@ export default function Header() {
             <h2>Web Designer & Front-end Developer</h2>
             <p className='hero-description'>I&rsquo;m an experienced Web Designer with <strong>5 years</strong> of expertise in designing, developing, and launching 100+ websites and landing pages, following the latest design trends to create impactful digital experiences.</p>
             <div className='hero-cta-grid'>
-                <a href={ResumePDF} target='_blank' className='view-resume'>View Resume</a>
-                <a href="#contact" className='view-resume ghost-cta'>Contact Now</a>
+                <a href={ResumePDF} target='_blank' className='view-resume'><span>Download CV</span></a>
+                <a href="#contact" className='ghost-cta'>Contact Now</a>
             </div>
           </div>
           <div className="pf-header-profile">
-            <img src={ProfileImage} alt="Profile Image" width='617' height='496' />
+            <img src={ProfileImage} alt="Profile Image" width='650' height='496' />
           </div>
         </div>
       </div>
