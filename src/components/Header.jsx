3be8c1c9d88dfe56be2ab3754296f9293e48798d
JensonR-Dev <r_jenson@outlook.com>
2025-02-04 05:43:24 +0530

    Hero section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,26 @@
+import ProfileImage from '../assets/Jenson-Profile.jpg';
+import ResumePDF from '../assets/Jenson-2024-Resume.pdf'
 
-export default function Header(){
-    return <header className="portfolio-header">
-        <div className="container">
-            <div className="pf-head-grid">
-                <div className="pf-head-cnt">
-                <h1>I&rsquo;m Jenson R</h1>
-                <p>Creative and detail-oriented Web Designer with 4 Years of experience translating client visions into innovative and visually appealing websites. Proficient in front-end development languages and tools such as HTML, CSS, JavaScript, Wordpress, Bootstrap, and Webflow, with a experience in design softwares like Abobe Photoshop, Illustrator and Figma. Adept at collaborating with cross-functional teams to deliver high-quality digital solutions that meet client objectives. Proven track record of managing multiple projects simultaneously, meeting tight deadlines, and exceeding client expectations. Passionate about staying abreast of industry trends and emerging technologies to consistently deliver cutting-edge designs.</p>
-                </div>
+export default function Header() {
+  return (
+    <header className="portfolio-hero">
+      <div className="container">
+        <div className="pf-head-grid">
+          <div className="pf-head-cnt">
+            <p className='greeting-msg'>Greetings </p>
+            <h1>I&rsquo;m <span className='hero-name'>Jenson R</span></h1>
+            <h2>Web Designer & Front-end Developer</h2>
+            <p className='hero-description'>I&rsquo;m an experienced Web Designer with <strong>5 years</strong> of expertise in designing, developing, and launching 100+ websites and landing pages, following the latest design trends to create impactful digital experiences.</p>
+            <div className='hero-cta-grid'>
+                <a href={ResumePDF} target='_blank' className='view-resume'>View Resume</a>
+                <a href="#contact" className='view-resume ghost-cta'>Contact Now</a>
             </div>
+          </div>
+          <div className="pf-header-profile">
+            <img src={ProfileImage} alt="Profile Image" width='617' height='496' />
+          </div>
         </div>
+      </div>
     </header>
+  );
 }
