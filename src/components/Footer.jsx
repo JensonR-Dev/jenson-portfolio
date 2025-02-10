491629cf1bf27cb8f51d7ed30fdf8e7e703f082a
JensonR-Dev <r_jenson@outlook.com>
2025-02-10 21:10:54 +0530

    footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+    return <footer className="pf-footer">
+        <div className="container">
+            <div className="pf-footer-grid">
+                <p className="footer-copyright">Copyright © 2025 <span>Jenson R</span>. All Rights Reservced.</p>
+                <p className="footer-powerby">Power by <span>React.js</span></p>
+            </div>
+        </div>  
+    </footer>
+}
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,20 @@
+import Logo from '../../assets/Jenson-logo.svg'
+import ResumePDF from "../../assets/Jenson-2024-Resume.pdf";
 import "./Navbar.css"
 
 export default function Navbar(){
-    return <nav>
-       
+    return <nav className="pf-navbar">
+       <ul className='nav-ul'>
+       <li><a href={ResumePDF} target='_black'><img src={Logo} alt="Jenson" width='50' height='50' className='nac-logo' /></a></li>
+       <div className="rgt-nav">
+         <li><a href="#about">About</a></li>
+         <li><a href="#about">Education</a></li>
+         <li><a href="#about">Work</a></li>
+         <li><a href="#about">Services</a></li>
+         <li><a href="#about">Projects</a></li>
+         <li><a href="#about">Skills</a></li>
+         <li><a href="#about" className='nav-cv'>View CV</a></li>
+       </div>
+       </ul>
     </nav>
 }
